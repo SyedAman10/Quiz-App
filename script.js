592075978839e5c46fda682b530f595286fc641d
Syed Aman <amanullahnaqvi@gmail.cpm>
2022-01-27 19:20:35 +0500

    setup

---
+++ script.js
@@ -0,0 +1,40 @@
+const quizData = [
+    {
+         question: 'How old is Aman?' ,
+         a:'10',
+         b:'17',
+         c:'26',
+         d:'21',
+         correct:'d'
+        
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a:'Java',
+        b:'C',
+        c:'Python',
+        d:'JavaScript',
+        correct:'d'
+    }, {
+        question: 'Who is the president of US?',
+        a:'Syed Aman',
+        b:'Donald Trump',
+        c:'Michael Jordan',
+        d:'Joe Biden',
+        correct:'d'
+    }, {
+        question: 'What does HTML stand for ?',
+        a:'Hypertext Markup Language',
+        b:'Hypertext Markups Language',
+        c:'Hyper Markup Language',
+        d:'Hypertext Mark Language',
+        correct:'a'
+    },
+    {
+        question: 'What year was Javascript launched?',
+        a:'1996',
+        b:'1995',
+        c:'2018',
+        d:'none of above',
+        correct:'d'
+    }
+];
